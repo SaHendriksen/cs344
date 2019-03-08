ba9775827db457ded288f4b2a2ded38cc2e40b70
SaHendriksen <sarah@jeffersondrive.org>
2019-03-08 15:33:28 -0500

    update lab06

--- lab06/lab06_3.py
+++ lab06/lab06_3.py
@@ -1,1 +1,4 @@
-# Keras wasn't working for me or my partner so I could not do this part
+# Keras wasn't working for me or my partner so I could not do this part. I tried the virtual environment fix and it
+# didn't work
+
+from keras.datasets import boston_housing
